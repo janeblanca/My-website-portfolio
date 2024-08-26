1db0e34ea79f8c3dd70ac0b1f809278e3b1dcae0
janeblanca <janeblancaocampo@gmail.com>
2024-08-26 09:46:20 +0800

    updated home screen

--- src/screens/HomeScreen/Home.js
+++ src/screens/HomeScreen/Home.js
@@ -1,27 +1,28 @@
 import React from "react";
-import '../HomeScreen/Home.css';
+import "../HomeScreen/Home.css";
 
-// Import my personal image 
-import MyImage from "../../assets/me.jpg"; 
+// Import my personal image
+import MyImage from "../../assets/me.jpg";
 
-export default function Home() { 
-    return (
-        <div className="homeSizePage">
-            <div className="infoAboutMe">
-                <div className="websiteOptions">
-                    <div className="optionsContainer">
-                        <p>HELLO</p>
-                        <p>PORTFOLIO</p>
-                        <p>MY JOURNEY</p>
-                        <p>CONTACT</p>
-                    </div>
-                </div>
-            </div>
-            <div className="profileContainer">
-                <div className="grayProfileContainer">
-                    <img src={MyImage} alt="MyImage" className="myImage"/>
-                </div>
-            </div>
+export default function Home() {
+  return (
+    <div className="homePage">
+      <div className="profileDetailsContainer">
+        <div className="webPageSectionContainer">
+          <div className="webPageSection">
+            <p className="webSectionText">HOME</p>
+            <p className="webSectionText">PORTFOLIO</p>
+            <p className="webSectionText">MY JOURNEY</p>
+            <p className="webSectionText">CONTACT</p>
+          </div>
         </div>
-    );
+        <div className="detailsContainer">
+          <div className="grayBox"></div>
+        </div>
+      </div>
+      <div className="profilePictureContainer">
+        <img src={MyImage} alt="MyImage" className="myProfilePicture" />
+      </div>
+    </div>
+  );
 }
